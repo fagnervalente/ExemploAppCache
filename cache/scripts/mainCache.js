7aaf170134126e05b7d71e62f19370bde171644a
Fagner Valente <fagnervalente@gmail.com>
2015-04-26 01:49:20 -0300

    versão funcionando

--- cache/scripts/mainCache.js
+++ cache/scripts/mainCache.js
@@ -1,43 +1,80 @@
 var appCache = {};
 
-appCache.url = {
-	server: 'http://localhost:3000/',
-	indisponivel: 'http://localhost:3000/cache/indisponivel.html',
-	offline: 'http://localhost:3000/cache/offline.html'
-};
+appCache.url = {};
 
+appCache.url.base = 'http://localhost:3000/';
+appCache.url.server = appCache.url.base;
+appCache.url.serverPing = appCache.url.base + 'ping';
+appCache.url.indisponivel = appCache.url.base + 'cache/indisponivel.html';
+appCache.url.offline = appCache.url.base + 'cache/offline.html';
 
+appCache.INTERVAL_TIME = 3000;
 appCache.isServerOnline = false;
 
-appCache.checkStatusApp = function(disponivel){
+appCache.startCheckStatusApp = function(){
 
-	appCache.isServerOnline = (disponivel) ? true : false;
+	appCache.checkStatusApp( appCache.setServerOn, appCache.setServerOff, appCache.setClientConnectionOff );
 
-	var intervalCheckStatusApp = setInterval(function(){
+	appCache.INTERVAL_CHECK_STATUS_APP = setInterval(function(){
+			appCache.checkStatusApp( appCache.setServerOn, appCache.setServerOff, appCache.setClientConnectionOff );
+		},
+		appCache.INTERVAL_TIME
+	);
+};
+appCache.setServerOn = function(online){
+	appCache.isServerOnline = true;
+	if ( window.parent.location.href != appCache.url.server)
+		window.parent.location = appCache.url.server;
+};
+appCache.setServerOff = function(online){
+	appCache.isServerOnline = false;
+	if ( window.parent.location.href != appCache.url.indisponivel)
+		window.parent.location = appCache.url.indisponivel;
+};
+appCache.setClientConnectionOff = function(){
+	if (window.parent.location.href != appCache.url.offline)
+		window.parent.location = appCache.url.offline;
+};
+appCache.checkStatusApp = function(callback_online, callback_offiline, callback_client_connection_off){
 
-		appCache.isOnLine = navigator.onLine;
+	callback_online = typeof(callback_online) === 'function' ? callback_online : new Function();
+	callback_offiline = typeof(callback_offiline) === 'function' ? callback_offiline : new Function();
+	callback_client_connection_off = typeof(callback_client_connection_off) === 'function' ? callback_client_connection_off : new Function();
 
-		if (appCache.isOnLine)
-		{
+	if (appCache.isOnline())
+	{
+		$.ajax({
+			url: 'http://localhost:3000/ping',
+			dataType: 'JSON',
+			success: callback_online,
+			error: callback_offiline
+		});
 
-			$.ajax({
-				url: 'http://localhost:3000/ping',
-				dataType: 'JSON',
-				success: function(){
-					appCache.isServerOnline = true;
-					if ( window.parent.location.href != appCache.url.server)
-						window.parent.location = appCache.url.server;
-				},
-				error: function(xhr, status, message){
+	}else{
+		callback_client_connection_off();
+	}
+};
+appCache.isOnline = function(){
 
-					appCache.isServerOnline = false;
-					if ( window.parent.location.href != appCache.url.indisponivel)
-						window.parent.location = appCache.url.indisponivel;
-				}
-			});
+	if ( 'onLine' in window.navigator ){
 
-		}else if (window.parent.location.href != appCache.url.offline) {
-			window.parent.location = appCache.url.offline;
+		return navigator.onLine;
+
+	}else{
+
+		var xhr = new XMLHttpRequest();
+		var onLine;
+
+		// phone home
+		xhr.open('HEAD', '/', false); // async=false
+		try {
+			xhr.send();
+			onLine = true;
+		} catch (e) {
+			// throws NETWORK_ERR when disconnected
+			onLine = false;
 		}
-	}, 3000);
+
+		return onLine;
+	}
 };
